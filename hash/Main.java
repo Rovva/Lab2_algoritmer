e59894597560d6a3525763dfd18155df843e62a1
Rovva <rovva@hostarn.se>
2016-11-21 15:07:54 +0100

    Början på slutet

--- hash/Main.java
+++ hash/Main.java
@@ -1,9 +1,15 @@
 package hash;
 
+import hash.NumberGenerator;
+import hash.Probe_1;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		NumberGenerator num = new NumberGenerator(7);
+		int arraySize = 6;
+		Probe_1 One = new Probe_1(arraySize, num);
 
 	}
 
--- hash/Probe_1.java
+++ hash/Probe_1.java
@@ -1,7 +1,18 @@
 package hash;
 
+import hash.NumberGenerator;
+
 public class Probe_1 {
 	
+	NumberGenerator num;
 	
+	public Probe_1(int x, NumberGenerator num) {
+		this.num = num;
+	}
+	
+	public void hash(int x) {
+		
+		
+	}
 
 }
