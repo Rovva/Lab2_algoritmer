43e3fc80561a06a16378dab582fae58e068ad485
Beldrama <roininin4@hotmail.com>
2016-11-26 17:58:53 +0100

    New insertion 1.2 and experiment prints
    Nu är det bara excel kvar och rapport

--- hash/HashProbe.java
+++ hash/HashProbe.java
@@ -9,25 +9,28 @@ public class HashProbe {
 	NumberGenerator num;
 	int arraySize;
 	int[] HashTable;
-	int up;
-	int down;
+	String [] statusTable; 
+	int down1;
+	int up1;
 	int c;
 	int [] lastRehash;
 	int nrRehash;
-	int loadFactor;
 	int nrInsertcollision;
 	int longestChain;
 	int currentChain;
+	int probes;
 	
 	public HashProbe(int x, NumberGenerator num, int c) {
 		this.num = num;
 		this.arraySize = x;
 		this.HashTable = new int[arraySize];
-		up = 0;
-		down = 0;
+		this.statusTable = new String[arraySize];
+		probes = 0;
 		this.c = c;
 		this.lastRehash = new int[1];
 		this.nrRehash = 0;
+		this.down1 = 0;
+		this.up1 = 0;
 
 	}
 	
@@ -50,14 +53,14 @@ public class HashProbe {
 			
 		}
 		if(i > 0){
-			currentChain++;
+			currentChain = i;
 			nrInsertcollision++;
-		} else {
+			probes += i;
+			
 			if(currentChain > longestChain){
-				int diff = currentChain - longestChain;
-				longestChain += diff; 
+				longestChain = currentChain;
 			}
-			currentChain = 0;
+
 		}
 		
 	}
@@ -69,7 +72,7 @@ public class HashProbe {
 		while (i != arraySize){
 			
 
-			if (down <= up){
+			if (down1 <= up1){
 				v = hash(x + i);
 			} else {
 				v = hash(x - i);
@@ -79,14 +82,14 @@ public class HashProbe {
 				
 				HashTable[v] = x;
 				
-				if (i != 0 && down <= up) {
+				if (i != 0 && down1 <= up1) {
 					
-					down++;
+					down1++;
 					break;
 					
-				} else if (i != 0 && down > up) {
+				} else if (i != 0 && down1 > up1) {
 					
-					up++;
+					up1++;
 					break;
 					
 				}
@@ -100,14 +103,86 @@ public class HashProbe {
 			
 		}
 		if(i > 0){
-			currentChain++;
+			currentChain = i;
 			nrInsertcollision++;
-		} else {
+			probes += i;
+			
 			if(currentChain > longestChain){
-				int diff = currentChain - longestChain;
-				longestChain += diff;
+				longestChain = currentChain;
 			}
-			currentChain = 0;
+
+		}
+		
+		
+		
+	}
+	
+	public void insert1_2(int x) {
+
+		int up = 0;
+		int down = 0;
+		int i = 0;
+		int v;
+		while (i != arraySize){
+			
+
+			if (i == 1){
+				
+				for(int y = 0; y < arraySize; y++ ){
+					if(HashTable[y] != 0 && hash(HashTable[y]) == hash(x)){
+						if (statusTable[y] == "down"){
+							down++;
+						}
+						else if (statusTable[y] == "up"){
+							up++;
+						}
+					}
+				}
+				
+			}
+
+			if (down <= up){
+				v = hash(x + i);
+			} else {
+				v = hash(x - i);
+			}
+			
+			
+			if (HashTable[v] == 0){
+				
+				HashTable[v] = x;
+				
+				
+				if (i != 0 && down <= up) {
+					
+					statusTable[v] = "down";
+					break;
+					
+				} else if (i != 0 && down > up) {
+					
+					statusTable[v] = "up";
+					break;
+					
+				}
+				statusTable[v] = "ok";
+				break;
+				
+			
+			} else {
+				i++;
+			}
+			
+			
+		}
+		if(i > 0){
+			currentChain = i;
+			nrInsertcollision++;
+			probes += i;
+			
+			if(currentChain > longestChain){
+				longestChain = currentChain;
+			}
+
 		}
 		
 		
@@ -132,11 +207,11 @@ public class HashProbe {
 					
 					for(int a = hash(x); a <= hash(x + c); a++){
 						
-						System.out.println("I AM IN THE FOR LOOP HOHOH");
+						//System.out.println("I AM IN THE FOR LOOP HOHOH");
 						
 						int y = HashTable[a];
 						if (y != 0 && Math.abs(j - hash(y)) <= c ){
-							System.out.println("SWITCHING a:" + HashTable[a] + " with j:" + x);
+							//System.out.println("SWITCHING a:" + HashTable[a] + " with j:" + x);
 							HashTable[j] = y;
 							HashTable[a] = x;
 							break;
@@ -147,7 +222,7 @@ public class HashProbe {
 						lastRehash = reHash(HashTable);
 						continue;
 					} else {
-						System.out.println("ReHasing fail!");
+						//System.out.println("ReHasing fail!");
 						break;
 					}
 					
@@ -162,16 +237,16 @@ public class HashProbe {
 			}
 			
 		}
+		
 		if(i > 0){
-			currentChain++;
+			currentChain = i;
 			nrInsertcollision++;
-		} else {
+			probes += i;
+			
 			if(currentChain > longestChain){
-				int diff = currentChain - longestChain;
-				longestChain += diff;
-				
+				longestChain = currentChain;
 			}
-			currentChain = 0;
+
 		}
 		
 		
@@ -179,7 +254,7 @@ public class HashProbe {
 	
 	public int[] reHash(int[] OldHashTable){
 		
-		System.out.println("REHASHING!!!!!!!!");
+		//System.out.println("REHASHING!!!!!!!!");
 		
 		int[] Old = Arrays.copyOf(OldHashTable, OldHashTable.length);
 		
@@ -204,29 +279,31 @@ public class HashProbe {
 	}
 	
 	public void getHash() {
+		System.out.println();
 		System.out.print("[");
 		for (int i = 0; i < arraySize; i++){
 			System.out.print(HashTable[i] + ", ");
 		}
 		System.out.print("]");
-		System.out.println("Load factor = " + loadFactor);
+		System.out.println();
 		System.out.println("C = " + c);
 		System.out.println("Number of rehash = " + nrRehash);
 		System.out.println("Number of insert with collision = " + nrInsertcollision);
 		System.out.println("Longest collision chain = " + longestChain);
+		System.out.println("Number of probes = " + probes);
 	}
 	
 	public void clearHash(){
 		
 		this.HashTable = new int[arraySize];
-		this.up = 0;
-		this.down = 0;
 		this.lastRehash = new int[1];
 		this.nrRehash = 0;
-		this.loadFactor = 0;
 		this.nrInsertcollision = 0;
 		this.longestChain = 0;
 	    this.currentChain = 0;
+	    this.probes = 0;
+	    this.down1 = 0;
+	    this.up1 = 0;
 		
 	}
 	
--- hash/Main.java
+++ hash/Main.java
@@ -10,15 +10,51 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		
 		long time;
 		long startTime;
-		int arraySize = 100;
-		int c = 10;
+
+		int c = 500;
+		int t = 10;
 		
+		for(int u = 1000; u <= 10000; u += 1000){
+
+			long tottime = 0;
+			long tottime1 = 0;
+			long tottime1_2 = 0;
+			long tottime2 = 0;
+			double totfact = 0;
+			int totprobe = 0;
+			int totprobe1 = 0;
+			int totprobe1_2 = 0;
+			int totprobe2 = 0;
+			int totrehash = 0;
+			int totcollision = 0;
+			int totcollision1 = 0;
+			int totcollision1_2 = 0;
+			int totcollision2 = 0;
+			int totchain = 0;
+			int totchain1 = 0;
+			int totchain1_2 = 0;
+			int totchain2 = 0;
+			int arraySize = u;
+			
+			System.out.println("");
+			System.out.println("-----------");
+			System.out.println("ArraySize: " + arraySize);
+			System.out.println("c: " + c);
+			System.out.println("-----------");
+			System.out.println("");
+
+		
+		
+
+		for (int k = 0; k < t; k++){
+			
 		
 		NumberGenerator num = new NumberGenerator(arraySize);
 		HashProbe One = new HashProbe(arraySize, num, c);
-		System.out.println(num.unsorted);
+		//System.out.println(num.unsorted);
 		
 		//INSERTION
 		
@@ -29,11 +65,19 @@ public class Main {
 
 		}
 		time = (long) ((System.nanoTime() - startTime));
-		System.out.println(time + " ns");
+		//System.out.println(time + " ns");
+		tottime += time;
 		
-		One.getHash();
+		totprobe += One.probes;
+		totcollision += One.nrInsertcollision;
+		totchain += One.longestChain;
+		
+		//One.getHash();
 		One.clearHash();
-		System.out.println("");
+		//System.out.println("");
+		
+
+		//System.out.println("-------------");
 		
 		
 		//INSERTION 1
@@ -46,11 +90,54 @@ public class Main {
 		}
 		
 		time = (long) ((System.nanoTime() - startTime));
-		System.out.println(time + " ns");
+		//System.out.println(time + " ns");
+		tottime1 += time;
 		
-		One.getHash();
+		totprobe1 += One.probes;
+		totcollision1 += One.nrInsertcollision;
+		totchain1 += One.longestChain;
+		
+		//One.getHash();
 		One.clearHash();
-		System.out.println("");
+		
+		//System.out.println("");
+		//System.out.println("-------------");
+		
+		//INSERTION 1.2
+		
+				startTime = System.nanoTime();
+				
+				for(int i = 0; i < arraySize; i++) {
+					One.insert1_2((int) num.unsorted.get(i));
+
+				}
+				
+				time = (long) ((System.nanoTime() - startTime));
+				//System.out.println(time + " ns");
+				tottime1_2 += time;
+				
+
+				totprobe1_2 += One.probes;
+				totcollision1_2 += One.nrInsertcollision;
+				totchain1_2 += One.longestChain;
+				
+				//One.getHash();
+
+				//System.out.print("[");
+				
+				//for(int i = 0; i < arraySize; i++) {
+					//System.out.print(One.statusTable[i] + ", ");
+				//}
+
+				//System.out.print("]");
+				//System.out.println();
+				
+				One.clearHash();
+				
+				//System.out.println("-------------");
+				
+				
+				
 		
 		//INSERTION 2
 		
@@ -64,11 +151,59 @@ public class Main {
 		}
 		
 		time = (long) ((System.nanoTime() - startTime));
-		System.out.println(time + " ns");
+		//System.out.println(time + " ns");
+		tottime2 += time;
 		
-		System.out.println("Load: " + (One.checkLoad() * 100) + "%");
-		One.getHash();
+		totfact += One.checkLoad();
+		totrehash += One.nrRehash;
+		totprobe2 += One.probes;
+		totcollision2 += One.nrInsertcollision;
+		totchain2 += One.longestChain;
+		
+		//System.out.println("Load: " + (One.checkLoad() * 100) + "%");
+		//One.getHash();
 		One.clearHash();
+
+		}
+		System.out.println("-----------");
+		System.out.println("Insert");
+		System.out.println("-----------");
+		System.out.println("Average time: " + (tottime / t)/1000000 + " milli seconds");
+		System.out.println("Average Number of probes: " + (totprobe / t));
+		System.out.println("Average Number of collisions: " + (totcollision / t));
+		System.out.println("Average longest chain: " + (totchain / t));
+		System.out.println("");
+		
+		System.out.println("-----------");
+		System.out.println("Insert1");
+		System.out.println("-----------");
+		System.out.println("Average time1: " + (tottime1 / t)/1000000  + " milli seconds");
+		System.out.println("Average Number of probes: " + (totprobe1 / t));
+		System.out.println("Average Number of collisions: " + (totcollision1 / t));
+		System.out.println("Average longest chain: " + (totchain1 / t));
+		System.out.println("");
+		
+		System.out.println("-----------");
+		System.out.println("Insert1.2");
+		System.out.println("-----------");
+		System.out.println("Average time1_2: " + (tottime1_2 /t)/1000000  + " milli seconds");
+		System.out.println("Average Number of probes: " + (totprobe1_2 / t));
+		System.out.println("Average Number of collisions: " + (totcollision1_2 / t));
+		System.out.println("Average longest chain: " + (totchain1_2 / t));
+		System.out.println("");
+		
+		System.out.println("-----------");
+		System.out.println("Insert2");
+		System.out.println("-----------");
+		System.out.println("Average time2: " + (tottime2 / t)/1000000  + " milli seconds");
+		System.out.println("Average Number of probes: " + (totprobe2 / t));
+		System.out.println("Average Number of collisions: " + (totcollision2 / t));
+		System.out.println("Average longest chain: " + (totchain2 / t));
+		System.out.println("Average loadfactor: " + (totfact / t)*100 + "%");
+		System.out.println("Average Number of ReHash: " + (totrehash / t));
+		
+		
+		}
 		
 	}
 
